287510feed6eb7151093630764c4075b9333f352
krishnaji-eGov <87766660+krishnaji-eGov@users.noreply.github.com>
2022-04-02 10:28:07 +0530

    first commit

---
+++ android/app/src/main/kotlin/incitesol/sales/RetailOps/tracker_map/MainActivity.kt
@@ -0,0 +1,6 @@
+package incitesol.sales.RetailOps.tracker_map
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/tracker.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_background_geolocation/flutter_background_geolocation.dart' as bg;
+
+class Tracker extends StatefulWidget {
+  const Tracker({Key? key}) : super(key: key);
+
+  @override
+  _TrackerState createState() => _TrackerState();
+}
+
+class _TrackerState extends State<Tracker> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Fired whenever a location is recorded
+    bg.BackgroundGeolocation.onLocation((bg.Location location) {
+      print('[location] - $location');
+    });
+
+    // Fired whenever the plugin changes motion-state (stationary->moving and vice-versa)
+    bg.BackgroundGeolocation.onMotionChange((bg.Location location) {
+      print('[motionchange] - $location');
+    });
+
+    // Fired whenever the state of location-services changes.  Always fired at boot
+    bg.BackgroundGeolocation.onProviderChange((bg.ProviderChangeEvent event) {
+      print('[providerchange] - $event');
+    });
+
+    bg.BackgroundGeolocation.ready(bg.Config(
+        desiredAccuracy: bg.Config.DESIRED_ACCURACY_HIGH,
+        distanceFilter: 10.0,
+        stopOnTerminate: false,
+        startOnBoot: true,
+        debug: true,
+        logLevel: bg.Config.LOG_LEVEL_VERBOSE
+    )).then((bg.State state) {
+      if (!state.enabled) {
+        ////
+        // 3.  Start the plugin.
+        //
+        bg.BackgroundGeolocation.start();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    )
+  }
+}
